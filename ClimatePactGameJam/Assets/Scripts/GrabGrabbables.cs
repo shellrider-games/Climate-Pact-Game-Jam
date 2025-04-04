486ac1003e78791c5403811d87f3c2ecf92e1a21
Georg Becker <georg.becker@shellrider.com>
2025-04-04 23:33:52 +0200

    Pick up and drop prototype

---
+++ ClimatePactGameJam/Assets/Scripts/GrabGrabbables.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrabGrabbables : MonoBehaviour
+{
+    [SerializeField] private float hoverHeight = 0.1f;
+    [SerializeField] private LayerMask hoverableLayerMask;
+    private GameObject grabbedObject;
+
+    public void OnMainInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            TryGrab();
+        }
+
+        if (context.canceled)
+        {
+            TryRelease();
+        }
+    }
+
+    private void TryGrab()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("Grabbable"))
+            {
+                grabbedObject = hit.collider.gameObject;
+            }
+        }
+    }
+
+    private void TryRelease()
+    {
+        grabbedObject = null;
+    } 
+
+    private void MoveGrabbedObject()
+    {
+        if (grabbedObject == null) {return;}
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, hoverableLayerMask))
+        {
+            grabbedObject.transform.position = hit.point + Vector3.up * hoverHeight;
+        }
+    }
+    
+    public void Update()
+    {
+        MoveGrabbedObject();
+    }
+}
