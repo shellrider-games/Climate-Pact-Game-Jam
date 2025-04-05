80192b66a52cecef307cbbc9483ed994fbedf683
Georg Becker <georg.becker@shellrider.com>
2025-04-05 14:27:21 +0200

    CreateCombinedCollider

---
+++ ClimatePactGameJam/Assets/Scripts/CombinedCollider.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class CombinedCollider : MonoBehaviour
+{
+    private BoxCollider collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+        UpdateCombinedCollider();
+    }
+
+    private void UpdateCombinedCollider()
+    {
+        Bounds combinedBounds = new Bounds();
+
+        bool firstColliderFound = false;
+        foreach (Transform child in transform)
+        {
+            if (child.TryGetComponent(out Collider c) && c.enabled)
+            {
+                if (!firstColliderFound)
+                {
+                    combinedBounds = c.bounds;
+                }
+                firstColliderFound = true;
+                combinedBounds.Encapsulate(c.bounds);
+            }
+        }
+
+        if (firstColliderFound)
+        {
+            collider.center = transform.InverseTransformPoint(combinedBounds.center);
+            collider.size = combinedBounds.size;
+        }
+    }
+}
